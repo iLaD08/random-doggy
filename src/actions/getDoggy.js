2e3ef2e887ef5fca732e93af58cdf8ea2caa95c2
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-06-18 22:22:02 +0100

    fixed a glitch in getDoggy

--- src/actions/getDoggy.js
+++ src/actions/getDoggy.js
@@ -10,7 +10,16 @@ const getDoggy = ({ setDoggy, setLoading }) => {
       });
       setLoading(false);
     })
-    .catch((err) => console.error(err));
+    .catch(() => {
+      console.log("error");
+      axios.get("https://api.thedogapi.com/v1/images/search").then((res) => {
+        setDoggy({
+          name: res.data[0].breeds[0].name,
+          picture: res.data[0].url,
+        });
+        setLoading(false);
+      });
+    });
 };
 
 export default getDoggy;
